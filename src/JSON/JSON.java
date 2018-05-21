ee95ce18b09bb9785b3cc3b8b6f1f8137563a452
Mar, Mike <31870347+Mikegmar@users.noreply.github.com>
2018-05-20 21:23:22 -0700

    add to JSON.java

--- src/JSON/JSON.java
+++ src/JSON/JSON.java
@@ -4,45 +4,80 @@
  * and open the template in the editor.
  */
 package JSON;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+
 /**
  *
  * @author mikeg
  */
 public class JSON {
-    
-    public static void main(String[] args) {
-        
+
+    public static void main(String[] args) throws FileNotFoundException {
+
         Scanner input = new Scanner(System.in);
+
         System.out.print("Enter Student name: ");
         String name = input.nextLine();
-        System.out.print("Enter Course name: ");
-        String course1 = input.nextLine();
-        System.out.print("Enter Grade: ");
-        int grade1 = input.nextInt();
-        
-        
+
         //Create new JSON Object
-        JSONObject tuna =new JSONObject();
-        
+        JSONObject tuna = new JSONObject();
+
         //Name Value
         tuna.put("name", name);
-        
-        //Create Array/Object, Store class object
-        JSONObject classObject1 = new JSONObject();
-        classObject1.put("grade", grade1);
-        classObject1.put("name", course1);
-        
-        
+
         JSONArray courses = new JSONArray();
-        courses.add(classObject1);
-        
+
+        while (true) {
+
+            //get the course name
+            System.out.print("Enter Course name: ");
+            String course = input.nextLine();
+
+            //check to see fi user hit the enter button
+            if (course.length() == 0) {
+                break;
+            }
+
+            //get course grade
+            System.out.print("Enter Grade: ");
+            int grade = input.nextInt();
+
+            if (input.hasNextLine()) {
+                input.nextLine();
+            }
+
+            //Create Array/Object, Store class object
+            JSONObject classObject = new JSONObject();
+            classObject.put("grade", grade);
+            classObject.put("name", course);
+
+            //add to array
+            courses.add(classObject);
+        }
+
         //add array to tuna object
         tuna.put("courses", courses);
-        
+
         System.out.println(tuna.toJSONString());
-        
+
+        //Create file
+        File file = new File("StudentGrades.txt");
+
+        try {
+            PrintWriter writer;
+            writer = new PrintWriter(file);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(JSON.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
+
 }
