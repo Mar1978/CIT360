90e921c08a41f1f1ca17a096f9e2377b4e986b08
Mar, Mike <31870347+Mikegmar@users.noreply.github.com>
2018-05-28 21:49:20 -0700

    add

--- src/JavaCollections/mainCollections.java
+++ src/JavaCollections/mainCollections.java
@@ -4,9 +4,28 @@
  * and open the template in the editor.
  */
 package JavaCollections;
+
+import java.io.*;
+import java.util.*;
+
 /**
  *
  * @author mikeg
  */
 public class mainCollections {
+
+    public static void main(String[] args) {
+         // Creating array, vector and hashtable
+        int arr[] = new int[]{1, 2, 3, 4};
+        Vector<Integer> vec = new Vector();
+        Hashtable<Integer, String> hash = new Hashtable();
+        vec.addElement(1);
+        vec.addElement(2);
+        hash.put(1, "super");
+        hash.put(2, "super star");
+
+        System.out.println(arr[0]);
+        System.out.println(vec.elementAt(0));
+        System.out.println(hash.get(2));
+    }
 }
